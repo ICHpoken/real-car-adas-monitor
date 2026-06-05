b180192c8906fd028747949a29b7d27e99d0a7df
ICHpoken <misha_p2003@mail.ru>
2026-06-05 11:23:21 +0300

    feat: initial project structure and CMake setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#define APP_VERSION "1.0.0"
+
+int main()
+{
+    std::cout << "Welcome to RealCarMonitor!" << std::endl;
+    std::cout << "Application version: " << APP_VERSION << std::endl;
+
+    return 0;
+}
